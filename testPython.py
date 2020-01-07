c67303da2d795ffb34355280745aa696748ce347
Brittany Lane <bll28@students.uwf.edu>
2020-01-07 10:04:00 -0600

    This is a starter file

---
+++ testPython.py
@@ -0,0 +1,19 @@
+print("This is a python test")
+x = 9
+y = 10
+
+print(x)
+
+if x > y:
+    print("I guess x be greater than y idk.")
+
+if y > x:
+    print("I guess y be greater than x idk.")
+
+    
+    
+z = x + y
+
+print(z)
+
+print("hello darkness my old friend")
